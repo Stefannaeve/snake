6d7dfbc5649d9497f624c7639b6d8aa8ada36259
stefan <stefan.naeve@hotmail.com>
2025-01-10 20:56:49 +0100

    Add game file for further refactoring and development

---
+++ include/game.h
@@ -0,0 +1,8 @@
+//
+// Created by stefan on 1/10/25.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+#endif //GAME_H
---
+++ src/game.c
@@ -0,0 +1,5 @@
+//
+// Created by stefan on 1/10/25.
+//
+
+#include "../include/game.h"
